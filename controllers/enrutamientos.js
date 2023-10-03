47870019281cf905479e1697f5c27ccd07ffe3fb
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-10-02 21:29:36 -0300

    Add files via upload

---
+++ controllers/enrutamientos.js
@@ -0,0 +1,3 @@
+document.getElementById("irAlTateti").addEventListener("click", function() {
+    window.location.href = "/views/tateti.html";
+});
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 addEventListener
+
+
+
+
+
+/*
+let Matriz = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
+  
+  let jugador = 1;
+
+const matrizElement = document.getElementById('matriz');
+
+matrizElement.addEventListener('click', function(event) {
+  if (event.target.tagName === 'TD') {
+    const contenidoCelda = event.target.textContent;
+    if (contenidoCelda === '') {
+      event.target.textContent = jugador === 1 ? 'X' : 'O';
+      jugador = jugador === 1 ? 2 : 1;
+    }
+  }
+});
+*/
