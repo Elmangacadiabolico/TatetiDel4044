1bfef1e8b2f4fa6060b8545a91ac37502980afcb
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-10-12 17:53:07 -0300

    Delete controllers/Controller.js

--- controllers/Controller.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (event) => {
-    const audio = document.getElementById("backgroundAudio");
-  
-    audio.volume = 1.0;
-    audio.play("/musicadefondo.wav");
-    audio.volume = volumeSlider.value;
-});
-
-
